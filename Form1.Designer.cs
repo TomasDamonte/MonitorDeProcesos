8187f09878c7039f09df558276d0c94d64a2a64a
tomas damonte <tomasdamonte@hotmail.com>
2021-08-11 13:35:21 -0300

    actualizacion

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,118 @@ namespace MonitorDeProcesos
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colNombre = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTotalProcessorTime = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colHilos = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colStartTime = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colWorkingSet64 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.behaviorManager1 = new DevExpress.Utils.Behaviors.BehaviorManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(12, 12);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(776, 426);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colId,
+            this.colNombre,
+            this.colTotalProcessorTime,
+            this.colHilos,
+            this.colStartTime,
+            this.colWorkingSet64});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.Editable = false;
+            // 
+            // colId
+            // 
+            this.colId.Caption = "Id";
+            this.colId.FieldName = "Id";
+            this.colId.Name = "colId";
+            this.colId.Visible = true;
+            this.colId.VisibleIndex = 0;
+            // 
+            // colNombre
+            // 
+            this.colNombre.Caption = "Nombre";
+            this.colNombre.FieldName = "ProcessName";
+            this.colNombre.Name = "colNombre";
+            this.colNombre.Visible = true;
+            this.colNombre.VisibleIndex = 1;
+            // 
+            // colTotalProcessorTime
+            // 
+            this.colTotalProcessorTime.Caption = "TotalProcessorTime";
+            this.colTotalProcessorTime.FieldName = "TotalProcessorTime";
+            this.colTotalProcessorTime.Name = "colTotalProcessorTime";
+            this.colTotalProcessorTime.Visible = true;
+            this.colTotalProcessorTime.VisibleIndex = 2;
+            // 
+            // colHilos
+            // 
+            this.colHilos.Caption = "Hilos";
+            this.colHilos.FieldName = "Hilos";
+            this.colHilos.Name = "colHilos";
+            this.colHilos.Visible = true;
+            this.colHilos.VisibleIndex = 3;
+            // 
+            // colStartTime
+            // 
+            this.colStartTime.Caption = "StartTime";
+            this.colStartTime.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom;
+            this.colStartTime.FieldName = "StartTime";
+            this.colStartTime.Name = "colStartTime";
+            this.colStartTime.Visible = true;
+            this.colStartTime.VisibleIndex = 4;
+            // 
+            // colWorkingSet64
+            // 
+            this.colWorkingSet64.Caption = "WorkingSet64 (MB)";
+            this.colWorkingSet64.FieldName = "WorkingSet64";
+            this.colWorkingSet64.Name = "colWorkingSet64";
+            this.colWorkingSet64.Visible = true;
+            this.colWorkingSet64.VisibleIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Monitor de procesos";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn colNombre;
+        private DevExpress.XtraGrid.Columns.GridColumn colId;
+        private DevExpress.XtraGrid.Columns.GridColumn colTotalProcessorTime;
+        private DevExpress.XtraGrid.Columns.GridColumn colHilos;
+        private DevExpress.XtraGrid.Columns.GridColumn colStartTime;
+        private DevExpress.XtraGrid.Columns.GridColumn colWorkingSet64;
+        private DevExpress.Utils.Behaviors.BehaviorManager behaviorManager1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -9,12 +11,93 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MonitorDeProcesos
-{
+{    
     public partial class Form1 : Form
     {
+        Timer Timer = new Timer();
+        BindingList<Proceso> Procesos = new BindingList<Proceso>();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        protected override void OnLoad(EventArgs e)
+        {            
+            gridControl1.DataSource = Procesos;
+            Timer.Interval = 1000;
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+            List<Process> prs = new List<Process>(Process.GetProcesses());
+            prs.ForEach(p => Procesos.Add(new Proceso(p)));
+            base.OnLoad(e);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {            
+            List<Process> prs = new List<Process>(Process.GetProcesses());
+
+            List<int> idsBorrar = new List<int>();
+            foreach (Proceso pro in Procesos)
+            {
+                if (!prs.Any(p => p.Id == pro.Id)) idsBorrar.Add(pro.Id);
+            }
+            for(int i = 0; i < idsBorrar.Count; i++)
+            {
+                Procesos.Remove(Procesos.First(p => p.Id == idsBorrar.ElementAt(i)));
+            }
+
+            foreach(Process pr in prs)
+            {
+                Proceso actual = Procesos.FirstOrDefault(p => p.Id == pr.Id);
+                if(actual != null) actual.Actualizar(pr);                
+                else Procesos.Add(new Proceso(pr));                              
+            }
+            gridView1.RefreshData();
+        }        
     }
+
+    public class Proceso
+    {
+        public int Id { get; set; }
+        public string ProcessName { get; set; }
+        public TimeSpan TotalProcessorTime { get; set; }
+        public int Hilos { get; set; }
+        public DateTime StartTime { get; set; }
+        public string WorkingSet64 { get; set; }
+
+        public Proceso() { }
+
+        public Proceso(Process p) 
+        {
+            Id = p.Id;            
+            ProcessName = p.ProcessName;
+            try { TotalProcessorTime = p.TotalProcessorTime; } catch (Exception) { }
+            Hilos = p.Threads.Count;
+            try { StartTime = p.StartTime; } catch (Exception) { }
+            WorkingSet64 = ToSize(p.WorkingSet64, SizeUnits.MB);            
+        }
+
+        public void Actualizar(Process p)
+        {
+            Id = p.Id;
+            ProcessName = p.ProcessName;
+            try { TotalProcessorTime = p.TotalProcessorTime; } catch (Exception) { }
+            Hilos = p.Threads.Count;
+            try { StartTime = p.StartTime; } catch (Exception) { }
+            WorkingSet64 = ToSize(p.WorkingSet64, SizeUnits.MB);
+        }
+
+        public enum SizeUnits
+        {
+            Byte, KB, MB, GB, TB, PB, EB, ZB, YB
+        }
+
+        public string ToSize(long value, SizeUnits unit)
+        {
+            return (value / (double)Math.Pow(1000, (long)unit)).ToString("0.00");
+        }
+    }
+
+
 }
